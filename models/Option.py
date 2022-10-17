27ac8613716b3bc650c03fb53a48424c86f2eb95
LBRUELL <linus.bruell@outlook.com>
2022-10-17 15:28:43 +0200

    Added a type check to maturity date

--- models/Option.py
+++ models/Option.py
@@ -1,5 +1,9 @@
-
 class Option:
+    # @requires
     def __init__(self, maturity_date, exercise_price) -> None:
+        #Validates if maturity date is of type str
+        if type(maturity_date) != 'str':
+            raise TypeError('maturity date should be string')
+
         self.maturity_date = maturity_date
         self.exercise_price = exercise_price
