116a6884ad3c8d39c1cac24b442f0764e2286d1c
LBRUELL <linus.bruell@outlook.com>
2022-10-21 11:56:54 +0200

    Class Option: -Further documentation -Implementation of an enum for the type

--- models/Option.py
+++ models/Option.py
@@ -1,9 +1,38 @@
+import string
+from enum import Enum
+
 class Option:
-    # @requires
-    def __init__(self, maturity_date, exercise_price) -> None:
-        #Validates if maturity date is of type str
+    def __init__(self, maturity_date, exercise_price, optionType) -> None:
+        """
+        Class for a stock option
+        @requires optionType to be either 'call' or 'put'
+        @param maturity_date: gives the date of maturity
+        @param exercise_price: gives the price for which the price is bought
+        @param optionType: gives the type of option
+        """
+
+        class Optiontype(Enum):
+            CALL = 1
+            PUT = 2
+
         if type(maturity_date) != 'str':
+            # Validates if maturity date is of type str
             raise TypeError('maturity date should be string')
+
+        if type(exercise_price) != 'int' or 'double':
+            # Validates if exercise price is of type integer or double
+            raise TypeError('exercise_prise must be integer or double')
+
+        if type(optionType) != 'str':
+            # Validates if option type is of type string
+            raise TypeError('optionType must be string')
 
         self.maturity_date = maturity_date
         self.exercise_price = exercise_price
+
+        if string.lower(optionType) == 'call':
+            self.optionType = Optiontype.CALL
+        elif string.optionType == 'put':
+            self.optionType = Optiontype.PUT
+        else:
+            raise AttributeError('Optiontype must be either call or put')
