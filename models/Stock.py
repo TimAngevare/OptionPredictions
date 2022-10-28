dae6da8074189c52791f03e657d9a47c6043c7ee
Jelle <jelle@famvandenbrink.net>
2022-10-28 14:50:37 +0200

    5->3 and future price in Stock

--- models/Stock.py
+++ models/Stock.py
@@ -23,7 +23,9 @@ class Stock:
         
         return rel_std_dev
     
-    def calc_growth_rate(self, eps_an, years):
+    #Calculate the growth rate of a stock
+    #eps_an = eps' of a period / periods = the periods / bs,fc,cf = balancesheet,financials,cashflow
+    def calc_growth_rate(self, eps_an, periods, bs, fc, cf):
         gr_values = {
             'Total Assets': 0.1,
             'Total Current Assets': 0.05,
@@ -35,53 +37,87 @@ class Stock:
         result = 0
 
         for i, (data, weight) in enumerate(gr_values.items()):
+            #Take the correct source based on the index since dicts are ordered
             if i < 3:
-                source = self.stock.balancesheet
+                source = bs
             elif i < 5:
-                source = self.stock.financials
+                source = fc
             else:
-                source = self.stock.cashflow
-            
+                source = cf
+                
             data_gr = 0
-            for j, year in enumerate(years):
-                if j == len(years) - 1:
+            for j, period in enumerate(periods):
+                #Don't run if we cant +1 the index in the same list
+                if j == len(periods) - 1:
                     break
-                    
-                present = source[year][data]
-                past = source[years[j + 1]][data]
+                
+                #Take present and past values of the thing
+                present = source[period][data]
+                past = source[periods[j + 1]][data]
+                #Calculate growth rate
                 data_gr += ((present - past) / past)
-            
-
+                
+            #Add the average growth rate * weight to the total
             result += ((data_gr / 2) * weight)
-        
-        #Specific for EPS
+            
+        #Add EPS growth rate specifically
         eps_gr = 0
         for i, eps in enumerate(eps_an):
-            if i == len(years) - 1:
+            if i == len(periods) - 1:
                 break
-                    
+                        
             past = eps_an[i + 1]
             eps_gr += ((eps - past) / past)
         result += ((eps_gr / 2) * 0.25)
 
-
         return result
 
-    def get_future_price(self):
-        historic = self.stock.history(start='2019-01-01', end='2021-12-31', interval='1mo')['Close']
+    #Calculate the future price of a stock
+    #when = period (either 1y/3y)
+    def get_future_price(self, when='3y'):
+        #setting the correct financial statement sheets
+        #settings the correct keyword arguments for the history period
+        #setting the correct list splice length for the periods
+        if when == '1y':
+            financials = self.stock.quarterly_financials
+            bs = self.stock.quarterly_balancesheet
+            cf = self.stock.quarterly_cashflow
+            kwargs = {'period': '1y'}
+            splice = 4
+        elif when == '3y':
+            financials = self.stock.financials
+            bs = self.stock.balancesheet
+            cf = self.stock.cashflow
+            kwargs = {'start':'2019-01-01', 'end':'2021-12-31'}
+            splice = 3
+        else:
+            print("'when' param should be 1y or 3y")
+            return -1
+
+        #Take the historic data / filter the NaN values / splice to the correct amount of periods
+        historic = self.stock.history(interval='1mo', **kwargs)['Close']
         fc = historic[historic.notnull()]
-        financial_years = self.stock.financials.keys()[:3]
+        periods = financials.keys()[:splice]
 
-        eps_an = [round(self.stock.financials[year]['Net Income Applicable To Common Shares'] / self.stock.shares['BasicShares'][year.year], 2) for year in financial_years]
+        #Calculate the EPS for every period in the periods list
+        eps_an = [round(financials[period]['Net Income Applicable To Common Shares'] / self.stock.shares['BasicShares'][period.year if period.year < 2022 else 2021], 2) for period in periods]
 
-        #Calculate future p/e ratio
-        closes_an = [fc[ts] for ts in fc.keys() if ts.month == 12]
-        closes_an.reverse()
+        # Calculate future p/e ratio
+        #take the relevant closing prices for the periods
+        if when == '1y':
+            closes = [fc[ts] for ts in fc.keys() if ts.month % 3 == 0]
+        elif when =='3y':
+            closes = [fc[ts] for ts in fc.keys() if ts.month == 12]
+        #in the correct order
+        closes.reverse()
 
-        tot_pe = [price / eps_an[i] for i, price in enumerate(closes_an)]
+        #calculate p/e ratio for every period
+        tot_pe = [price / eps_an[i] for i, price in enumerate(closes)]
+        #take the average p/e ratio
         pe = round(sum(tot_pe) / len(tot_pe), 2)
 
-        #Calculate future eps
-        eps = eps_an[0] * ((1 + self.calc_growth_rate(eps_an, financial_years)) ** len(financial_years))
+        #Calculate future eps (eps * (1 + growth)_rate^periods)
+        eps = eps_an[0] * ((1 + self.calc_growth_rate(eps_an, periods, bs, financials, cf)) ** len(periods))
 
+        #return future p/e ratio * eps
         return pe * eps
